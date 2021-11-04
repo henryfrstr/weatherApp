d06beb101e625e55699098cf140883c83258bde8
henryfrstr <henryfrstr@gmail.com>
2021-11-04 21:11:29 +0300

    feat: add City model

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import requests
+from decouple import config
+from pprint import pprint
 
 
 def index(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    city = "istanbul"
+    response = requests.get(url.format(city, config('API_KEY')))
+    content = response.json()
+    pprint(content)
+    print(type(content))
     return render(request, "weather_app/index.html")
